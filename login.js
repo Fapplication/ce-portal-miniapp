ddd27a246d6a95811dc24cb74d90a8d1d7e0fba9
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-05-22 04:12:38 -0700

    Update login.js

--- login.js
+++ login.js
@@ -16,7 +16,7 @@ document.getElementById("loginPassword").value
 try{
 
 const response = await fetch(
-"YOUR_WEB_APP_URL",
+"https://script.google.com/macros/s/AKfycbw4ogBvGCINM25cXPdxjnbobI1wEskVjtTVi4b_S2stwt6mNhYujuP_KMYfC2CoS5Z5/exec",
 {
 method:"POST",
 
