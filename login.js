5e433a355fa14b74b390749e075eafad29d3deb0
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-05-22 03:51:00 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,26 +1,39 @@
-const loginForm = document.getElementById("loginForm")
+const loginForm =
+document.getElementById("loginForm")
 
-loginForm.addEventListener("submit", async (e)=>{
+loginForm.addEventListener("submit",
+
+async (e)=>{
 
 e.preventDefault()
 
-const email = document.getElementById("loginEmail").value
+const email =
+document.getElementById("loginEmail").value
 
-const password = document.getElementById("loginPassword").value
+const password =
+document.getElementById("loginPassword").value
 
-const response = await fetch("https://script.google.com/macros/s/AKfycbw4ogBvGCINM25cXPdxjnbobI1wEskVjtTVi4b_S2stwt6mNhYujuP_KMYfC2CoS5Z5/exec",{
-
+const response = await fetch(
+"YOUR_WEB_APP_URL",
+{
 method:"POST",
 
 body:JSON.stringify({
+
 action:"login",
+
 email:email,
+
 password:password
-})
 
 })
+
+}
+)
 
 const result = await response.json()
+
+console.log(result)
 
 if(result.status == "success"){
 
@@ -29,7 +42,8 @@ localStorage.setItem(
 JSON.stringify(result.student)
 )
 
-window.location.href = "dashboard.html"
+window.location.href =
+"dashboard.html"
 
 }else{
 
