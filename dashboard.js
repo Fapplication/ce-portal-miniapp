b369e4107f6a81e83e9099ab6481cc23063234c7
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-06-02 04:43:27 -0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -42,7 +42,7 @@ async function sendComplaint(){
 const complaint =
 document.getElementById("complaint").value
 
-await fetch("https://script.google.com/macros/s/AKfycbw4ogBvGCINM25cXPdxjnbobI1wEskVjtTVi4b_S2stwt6mNhYujuP_KMYfC2CoS5Z5/exec",{
+await fetch("https://script.google.com/macros/s/AKfycbxAw2GI1gTjMFdutXHhd-IbIf345Le6ciZ-QXn8qAVztk5PrHwUjYfxuRxXZ3YUbzn2/exec",{
 
 method:"POST",
 
